e32fb006963661643948ad898889fb0319470e08
Erin Ewart <ems1980@hotmail.com>
2022-04-01 19:58:35 -0400

    added code to render license links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,47 +1,72 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (!license) {
-    return '';
-  }
+// function renderLicenseBadge(license) {
+//   if (!license) {
+//     return '';
+//   }
 
-  return `
-    <section class="my-3" id="about">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-      <p>${aboutText}</p>
-    </section>
-  `;
-}
+//   return `
+//     <section class="my-3" id="about">
+//       <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+//       <p>${aboutText}</p>
+//     </section>
+//   `;
+// }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (!license) {
     return '';
-  }
+  };
 
-  return `
-    <section class="my-3" id="about">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-      <p>${aboutText}</p>
-    </section>
-  `;
+  if (license == "GNU AGPLv3") {
+    return  "For more information about this license please visit [https://choosealicense.com/licenses/agpl-3.0/]"
+  };
+
+  if (license == "GNU GPLv3") {
+    return  "For more information about this license please visit [https://choosealicense.com/licenses/gpl-3.0//]"
+  };
+
+  if (license == "GNU LGPLv3") {
+    return  "For more information about this license please visit [https://choosealicense.com/licenses/lgpl-3.0//]"
+  };
+
+  if (license == "Mozilla Public License 2.0") {
+    return  "For more information about this license please visit [https://choosealicense.com/licenses/mpl-2.0/]"
+  };
+
+  if (license == "Apache License 2.0") {
+    return  "For more information about this license please visit [https://choosealicense.com/licenses/apache-2.0/]"
+  };
+
+  if (license == "MIT License") {
+    return  "For more information about this license please visit [https://choosealicense.com/licenses/mit/]"
+  };
+
+  if (license == "Boost Software License 1.0") {
+    return  "For more information about this license please visit [https://choosealicense.com/licenses/bsl-1.0/]"
+  };
+
+  if (license == "The Unlicense") {
+    return  "For more information about this license please visit [https://choosealicense.com/licenses/unlicense/]"
+  };
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (!license) {
-    return '';
-  }
+// function renderLicenseSection(license) {
+//   if (!license) {
+//     return '';
+//   }
 
-  return `
-    <section class="my-3" id="about">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-      <p>${aboutText}</p>
-    </section>
-  `;
-}
+//   return `
+//     <section class="my-3" id="about">
+//       <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+//       <p>${aboutText}</p>
+//     </section>
+//   `;
+// }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
